00b54bccbd3488fb2f1842fbf631253bc6ea1256
p-chanmin <pc_m111@naver.com>
2024-11-02 20:10:55 +0900

    feat: Admob content rating 설정

--- feature/main/src/main/java/com/dev/philo/fillsketch/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/dev/philo/fillsketch/feature/main/MainActivity.kt
@@ -18,6 +18,7 @@ import com.dev.philo.fillsketch.core.model.SoundEffect
 import com.dev.philo.fillsketch.feature.main.manager.AdMobManager
 import com.dev.philo.fillsketch.feature.main.manager.SoundEffectManager
 import com.google.android.gms.ads.MobileAds
+import com.google.android.gms.ads.RequestConfiguration
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -53,8 +54,13 @@ class MainActivity : ComponentActivity() {
 
         val soundEffectManager = SoundEffectManager(this, soundPool)
 
-        val backgroundScope = CoroutineScope(Dispatchers.IO)
-        backgroundScope.launch {
+        CoroutineScope(Dispatchers.IO).launch {
+            val requestConfiguration = MobileAds.getRequestConfiguration()
+                .toBuilder()
+                .setTagForChildDirectedTreatment(RequestConfiguration.TAG_FOR_CHILD_DIRECTED_TREATMENT_TRUE)
+                .setMaxAdContentRating(RequestConfiguration.MAX_AD_CONTENT_RATING_G)
+                .build()
+            MobileAds.setRequestConfiguration(requestConfiguration)
             MobileAds.initialize(this@MainActivity)
         }
         val adMobManager = AdMobManager(this)
